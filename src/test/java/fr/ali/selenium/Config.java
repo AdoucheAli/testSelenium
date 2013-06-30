da1821c00022a7b620e50ca5b7468223cd719e92
AdoucheAli <adoucheali@yahoo.fr>
2013-06-30 23:01:44 +0200

    last  commit

--- src/test/java/fr/ali/selenium/Config.java
+++ src/test/java/fr/ali/selenium/Config.java
@@ -1,7 +1,16 @@
 package fr.ali.selenium;
 
+import java.io.File;
+import java.text.MessageFormat;
+
 public final class Config {
     public static String getUrl() {
-        return "http://localhost:8080/TestSelenium";
+        return MessageFormat.format(
+                "http://localhost:8080/{0}", getProjectName());
+    }
+
+    public static String getProjectName(){
+    	File f = new File(System.getProperty("user.dir"));
+    	return f.getName();
     }
 }
